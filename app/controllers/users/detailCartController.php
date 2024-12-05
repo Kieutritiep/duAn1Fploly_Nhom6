fd819b57fd5a2a77cced0bb664d45f07b72a71c5
Kieutritiep <Kieutiep541@gmail.com>
2024-12-05 10:47:15 +0700

    Update detailCartController.php
    tiếp

--- app/controllers/users/detailCartController.php
+++ app/controllers/users/detailCartController.php
@@ -5,6 +5,10 @@
             $this->detailCart = new detailCartUserModel;
         }
         public function detailCartUser(){
-            require_once './views/users/cart/detailcart.php';
+            $idOrder = $_GET['id'];
+            $detailOrdersUsers = $this->detailCart->getDetailOrder($idOrder);
+            // var_dump($detailOrdersUsers);die();
+            // var_dump($orders);die();
+            require_once './views/users/cart/detailCart.php';
         }
     }
